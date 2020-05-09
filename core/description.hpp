e28559a1a9a839dd1e8032f06f42f5036125c2e0
Edward McKnight <eddy@em-creations.co.uk>
2020-05-09 17:20:17 +0100

    Update: Mission attributes moved to a dedicated file. Update: wreckManagerMode attribute introduced.

--- core/description.hpp
+++ core/description.hpp
@@ -3,41 +3,7 @@
 #ifdef description
 	#include "dia\rscdefinitions.hpp" // Must have for the end screen to work, if removed Arma 3 will crash on mission load
 
-	// DO NOT CHANGE, spectator script needs people to respawn, to be declared as dead and put into spectator mode
-	// See: https://community.bistudio.com/wiki/Description.ext#respawn
-	respawn = "BASE";
-
-	// 5 seconds are needed to make sure people properly die and go into spectator
-	// See: https://community.bistudio.com/wiki/Description.ext#respawnDelay
-	respawndelay = 5;
-
-	// disabledAI does so not slotted units do not spawn as ai
-	// See: https://community.bistudio.com/wiki/Description.ext#disabledAI
-	disabledAI = 1;
-
-	// Disables the respawn button
-	// See: https://community.bistudio.com/wiki/Description.ext#respawnButton
-	respawnButton = 1;
-
-	// Disables the score screen while respawning
-	// See: https://community.bistudio.com/wiki/Description.ext#respawnDialog
-	respawnDialog = 0;
-
-	// Disables respawn countdown
-	// See: https://community.bistudio.com/wiki/Description.ext#respawnTemplates
-	respawnTemplates[] = {"Base"};
-
-	// Only for logged-in admins
-	// See: https://community.bistudio.com/wiki/Description.ext#enableDebugConsole
-	enableDebugConsole = 1;
-
-	// Sets the mode for corpse removal manager. 1 = All units are managed by the manager
-	// See: https://community.bistudio.com/wiki/Description.ext#corpseManagerMode
-	corpseManagerMode = 1;
-
-	// Enable CBA Target Debugging
-	// See: https://github.com/CBATeam/CBA_A3/wiki/Target-Debugging
-	enableTargetDebug = 1;
+	#include "..\customization\missionAttributes.hpp"
 
 	class CfgDebriefingSections {
 		class acex_killTracker {
