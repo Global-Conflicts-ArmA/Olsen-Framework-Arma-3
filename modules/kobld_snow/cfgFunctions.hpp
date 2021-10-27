c8d0f54a6435494a11169ab858d18d8ec3379367
TheLocalPub <uncommon@hotmail.co.uk>
2021-10-27 14:03:40 +0100

    Added new module
    Added Tins Snow module which allows breath/stream from breathing, snow, fog, and winter post-processing effects.

---
+++ modules/kobld_snow/cfgFunctions.hpp
@@ -0,0 +1,12 @@
+class kobld_snow {
+	tag = "kobld_snow";
+
+	class General {
+		file = "modules\kobld_snow\functions";
+
+		class breathEffect {};
+		class posCheck {};
+		class shiver {};
+		class snowFX {};
+	};
+};
---
+++ modules/kobld_snow/script_component.hpp
@@ -0,0 +1,12 @@
+#define PREFIX kobld
+#define COMPONENT snow
+
+#ifdef DEBUG_ENABLED_MAIN
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_MAIN
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
+#endif
+
+#include "\x\cba\addons\main\script_macros_mission.hpp"
