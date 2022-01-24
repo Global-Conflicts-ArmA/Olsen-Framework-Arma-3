77caf1f8043eefcd94050cf22a0a650dae14266b
PiZZAD0X <bravoecholimalima@gmail.com>
2022-01-24 12:32:47 -0800

    add config entry macros for true and false, universalize macros for fnc

--- core/script_macros.hpp
+++ core/script_macros.hpp
@@ -10,9 +10,9 @@
 #define RNG(var1) var1 > random 1
 #define INVEHICLE(var1) (vehicle var1) isNotEqualTo var1
 #define RNDRANGE(var1,var2) var1 + (floor(random (1 + var2 - var1)))
-#define GETTEAMVAR(var1,var2,var3) private var1 = [var2, var3] call FUNC(GetTeamVariable)
+#define GETTEAMVAR(var1,var2,var3) private var1 = [var2, var3] call EFUNC(FW,GetTeamVariable)
 
-#define ERROR(MESSAGE) MESSAGE call FUNC(DebugMessage); \
+#define ERROR(MESSAGE) MESSAGE call EFUNC(FW,DebugMessage); \
 LOG_SYS('ERROR',MESSAGE)
 
 #define POS_X(N) ((N) * safezoneW + safezoneX)
