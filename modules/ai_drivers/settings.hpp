6a9f21311db48d65b3e7606de52db9f21eed407c
PiZZAD0X <bravoecholimalima@gmail.com>
2023-01-10 00:31:24 -0800

    Update settings.hpp

--- modules/ai_drivers/settings.hpp
+++ modules/ai_drivers/settings.hpp
@@ -21,12 +21,23 @@ FlipAction = false;
 topDisplay[] = {0.1,-0.22,0.5,0.25};
 bottomDisplay[] = {0.1,1,0.5,0.25};
 
-// Testmode for finding vehicle driver camera positions
+/* Test Mode for finding vehicle driver camera positions
+When TestMode is enabled vehicles will be given an ace interaction to enable (external action, when not in veh)
+movement and crouch/stand up keys will control the two orbs (yellow orb is the view position, red is target dir pos)
+spacebar will switch between controls moving the view (yellow orb) and the target (red orb)
+pressing escape will release the controls from the test mode action as well as copy the vehicle class type and position data
+you will be able to generate a config class below to correct the AI driver view with that data
+target dir pos will determine the angle of the camera in the driver view
+there is a driver view preview display as well as an AI driver that will spawn in so you can adjust around their model
+the view position doesn't need to be exactly where the driver is, up to the mission maker
+*/
 TestMode = true;
 
 // customized class config for AI driver view params
 class AIViewParams {
+    // vehicle class (it works with inheritance, so all M151 jeeps will inherit this config from the m151_base class)
     class vn_wheeled_m151_base {
+        // position data generated from test mode
         pos[] = {-0.5,0.04,-0.51};
         targetPos[] = {-0.51,0.38,-0.68};
     };
