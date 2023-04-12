584f5535ec9a7954d41f30877c18f0bcfbcc70de
PiZZAD0X <bravoecholimalima@gmail.com>
2023-04-12 09:58:55 -0700

    Update script_macros.hpp

--- core/script_macros.hpp
+++ core/script_macros.hpp
@@ -4,9 +4,9 @@
 #endif
 #define DEBUG_MODE_NORMAL
 //#define DEBUG_MODE_FULL
-#define VERSIONSTR "1.1.3"
+#define VERSIONSTR "1.1.4"
 #define VERSION 1.1
-#define VERSION_AR 1,1,3
+#define VERSION_AR 1,1,4
 #include "script_macros_mission.hpp"
 
 #define RNG(var1) var1 > random 1
