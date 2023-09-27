f043cc83570fbfa661d5e22fde5f45de43a202a6
PiZZAD0X <509thparachuteinfantry@gmail.com>
2023-09-27 12:58:22 -0700

    default commander off, default sightaid lower vision

--- modules/headless_ai/settings/sight.hpp
+++ modules/headless_ai/settings/sight.hpp
@@ -1,9 +1,9 @@
 // Aid to the AI spotting distance and time
 feature = true;
 // Aid to the AI spotting distance and time when in a vehicle
-vehicles = false;
+vehicles = true;
 // Distance at which the AI will start seeings enemies in LOS of them
-distance = 800;
+distance = 300;
 // Minimum reveal value per increase +n reveal value per check every 5 seconds of targets in LOS of enemies.
 minIncrease = 2;
 // Distance at which the AI will force engage the enemies
